a5bbcf89b74d16b8500288a77493c8d804f5a7b0
franzageek <franzageek2020@gmail.com>
2025-05-20 20:01:54 +0200

    Update Makefile, wire all together

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 CC=gcc
-CC_FLAGS=-g
+R_INCLUDE_DIRS=include
+CC_FLAGS=-g -I$(R_INCLUDE_DIRS)
 
-main.exe: main.c
+main: main.c input.c term.c magic.c table.c
 	$(CC) $^ -o $@ $(CC_FLAGS)
+
+clean:
+	rm main
--- main.c
+++ main.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+#include "magic.h"
+#include "table.h"
 
 int main(int argc, char** argv)
 {
-    printf("ciao mondo\n");
+    term_t* term = input__parse_args(argc, argv);
+    if (!term)
+    {
+        fprintf(stderr, "error: cannot allocate memory for command line arguments\n");
+        return 1;
+    }
+
+    term_t* aterm = term;
+    v64 count;
+    while (!term__is_null(aterm))
+    {
+        ++aterm; 
+        ++count;
+    }
+
+    --aterm;
+
+    do
+    {
+        if (aterm->power == 1)
+            term__print_coeff(aterm);
+
+        else if (aterm->power == 0)
+            printf("%+d", aterm->coeff);
+
+        else
+        {
+            term__print_coeff(aterm);
+            if (aterm->coeff != 0) printf("^%d", aterm->power);
+        }
+
+    } while ((aterm--)->power != 0);
+    printf("\n");
+
+    v64 magic = magic__find(term);
+    if (magic == 0)
+    {
+        fprintf(stderr, "error: cannot find magic number\n");
+        return 1;
+    }
+    printf("magic = %lld\n", magic);
+    
+    term_t* new = table__run_division(term, magic);
+
+    aterm = new;
+    printf("(x%+lld)(", -magic);
+    do
+    {
+        if (aterm->power == 1)
+            term__print_coeff(aterm);
+
+        else if (aterm->power == 0)
+            printf("%+d", aterm->coeff);
+
+        else
+        {
+            term__print_coeff(aterm);
+            if (aterm->coeff != 0) printf("^%d", aterm->power);
+        }
+
+        ++aterm;
+    } while (!term__is_null(aterm));
+    printf(")\n");
+    free(term);
+    free(new);
     return 0;
 }
